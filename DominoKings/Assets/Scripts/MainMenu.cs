b1047e1e1cf4fd3704763343e22ff49260385aeb
alex23job <106354657+alex23job@users.noreply.github.com>
2025-02-14 01:24:10 +0300

    Adding MainMenu

---
+++ DominoKings/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Text[] arTxtRecItems;
+
+    [SerializeField] private RawImage riAvatar;
+    [SerializeField] private Text txtName;
+    [SerializeField] private Text txtScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
